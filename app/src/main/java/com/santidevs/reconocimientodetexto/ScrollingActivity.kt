5f9ec7c4ff7085f5eac3d93563e81a595c7db4c2
SantiRuiz7 <ruizsantiagodavid@gmail.com>
2024-01-19 16:50:48 -0300

    Agregado a ScrollingActivity Letras Buttons

--- app/src/main/java/com/santidevs/reconocimientodetexto/ScrollingActivity.kt
+++ app/src/main/java/com/santidevs/reconocimientodetexto/ScrollingActivity.kt
@@ -1,8 +1,6 @@
 package com.santidevs.reconocimientodetexto
 
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.santidevs.reconocimientodetexto.databinding.ActivityScrollingBinding
